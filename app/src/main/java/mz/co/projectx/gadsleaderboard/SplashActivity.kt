14aafaa696b20e4f9b7b3ff1da26cb0888b436ab
Bernardo Maluleque <b.a.maluleque@gmail.com>
2020-09-10 02:00:57 +0200

    feat: Add Splash Screen

---
+++ app/src/main/java/mz/co/projectx/gadsleaderboard/SplashActivity.kt
@@ -0,0 +1,25 @@
+package mz.co.projectx.gadsleaderboard
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_splash)
+
+        val handler = Handler()
+        handler.postDelayed(Runnable {
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+
+    }
+
+}
